db051710129453fbc57101e0e097d7280448d828
fattiger <nobody0036@proton.me>
2024-12-04 01:27:22 +0800

    appchange

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -6,4 +6,7 @@ git clone https://github.com/sirpdboy/luci-app-ddns-go package/new/ddnsgo
 # wechatpush
 git clone https://github.com/tty228/luci-app-wechatpush.git package/new/luci-app-wechatpush
 #git clone https://github.com/gitbruc/luci-app-wechatpush.git package/new/luci-app-wechatpush
+#passwall
+rm -rf package/new/helloworld/{luci-app-passwall,patch-luci-app-passwall.patch}
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/new/helloworld/luci-app-passwall
 # 自定义脚本
