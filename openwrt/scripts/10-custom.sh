2aa8a7cd69b86ca1284f780886dc7f67a731675b
fattiger <nobody0036@proton.me>
2024-12-04 01:18:23 +0800

    appchange

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
+# onliner
+git clone https://github.com/gitbruc/luci-onliner.git package/new/luci-app-onliner
+# ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/new/ddnsgo
+# wechatpush
+git clone https://github.com/tty228/luci-app-wechatpush.git package/new/luci-app-wechatpush
+#git clone https://github.com/gitbruc/luci-app-wechatpush.git package/new/luci-app-wechatpush
 # 自定义脚本
