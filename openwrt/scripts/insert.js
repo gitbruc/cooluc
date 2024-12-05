2e2df71a962f97ac1426f330fb68dbead2355f48
fattiger <nobody0036@proton.me>
2024-12-05 20:09:28 +0800

    lucichange

--- openwrt/scripts/insert.js
+++ openwrt/scripts/insert.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 
-const filePath = './feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js';
 const newCode = `		// 添加新的行，文本居中，颜色为绿色
         var newRow = E('tr', { 'class': 'tr' });
         var newCell = E('td', { 'class': 'td center', 'colspan': '2', 'style': 'color: green;' },
@@ -10,19 +9,30 @@ const newCode = `		// 添加新的行，文本居中，颜色为绿色
         table.appendChild(newRow); // 将新行添加到表格
 `;
 
-try {
-  const fileContent = fs.readFileSync(filePath, 'utf-8');
-  const lines = fileContent.split('\n');
-  const returnTableIndex = lines.findIndex(line => line.trim() === 'return table;');
+function modifyFile(filePath) {
+  try {
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const lines = fileContent.split('\n');
+    const returnTableIndex = lines.findIndex(line => line.trim() === 'return table;');
 
-  if (returnTableIndex === -1) {
-    console.error('Could not find "return table;"');
-  } else {
-    lines.splice(returnTableIndex, 0, newCode); // 去除 .trim()
-    const updatedFileContent = lines.join('\n');
-    fs.writeFileSync(filePath, updatedFileContent);
-    console.log('Code inserted successfully!');
+    if (returnTableIndex === -1) {
+      console.error(`Could not find "return table;" in ${filePath}`);
+      return; // Exit early if "return table;" is not found
+    } else {
+      lines.splice(returnTableIndex, 0, newCode);
+      const updatedFileContent = lines.join('\n');
+      fs.writeFileSync(filePath, updatedFileContent);
+      console.log(`Code inserted successfully into ${filePath}!`);
+    }
+  } catch (err) {
+    console.error(`Error modifying ${filePath}:`, err);
   }
-} catch (err) {
-  console.error('Error:', err);
 }
+
+
+const filePaths = [
+  './feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js',
+  './package/system/autocore/files/generic/10_system.js'
+];
+
+filePaths.forEach(filePath => modifyFile(filePath));
