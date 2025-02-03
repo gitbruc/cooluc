93e7e2698662ef5ee6f5fb37565993b5bde8fe31
fattiger <nobody0036@proton.me>
2025-02-03 17:13:30 +0800

    shortcut

--- openwrt/scripts/00-prepare_base.sh
+++ openwrt/scripts/00-prepare_base.sh
@@ -68,7 +68,7 @@ rm -rf package/utils/util-linux
 git clone https://$github/sbwml/package_utils_util-linux -b openwrt-24.10 package/utils/util-linux
 
 # Shortcut Forwarding Engine
-git clone https://$gitea/sbwml/shortcut-fe package/new/shortcut-fe
+git clone https://github.com/gitbruc/shortcut-fe package/new/shortcut-fe
 
 # Patch FireWall 4
 if [ "$version" = "dev" ] || [ "$version" = "rc2" ]; then
