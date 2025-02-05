aa461af5adf141fc52b87882a6957e10acaf7d60
fattiger <nobody0036@proton.me>
2025-02-06 00:53:36 +0800

    +bbr

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -6,6 +6,8 @@ git clone https://github.com/sirpdboy/luci-app-ddns-go package/new/ddnsgo
 # wechatpush
 git clone https://github.com/tty228/luci-app-wechatpush.git package/new/luci-app-wechatpush
 #git clone https://github.com/gitbruc/luci-app-wechatpush.git package/new/luci-app-wechatpush
+# bbrswitch
+git clone https://github.com/gitbruc/openwrt-BBR.git package/new/luci-app-bbrswitch
 # passwall
 rm -rf package/new/helloworld/{luci-app-passwall,patch-luci-app-passwall.patch}
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/new/helloworld/luci-app-passwall
