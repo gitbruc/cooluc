2c1594b49d2cfe65dfbfa881d13dd437a7936a92
fattiger <nobody0036@proton.me>
2025-04-26 19:00:03 +0800

    smartdns

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -3,7 +3,7 @@
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/luci/applications/luci-app-smartdns
 git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
-git clone https://github.com/pymumu/luci-app-smartdns.git package/new/luci-app-smartdns
+git clone https://github.com/gitbruc/luci-app-smartdns.git package/new/luci-app-smartdns
 # wrtbwmon
 #git clone https://github.com/gitbruc/openwrt-wrtbwmon.git package/new/luci-app-wrtbwmon
 # onliner
