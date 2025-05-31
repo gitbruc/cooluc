f822d2bc4d1f1aa881e05c10944cba81e255ec1e
fattiger <nobody0036@proton.me>
2025-05-31 22:15:43 +0800

    fix gcc15 -c23

--- openwrt/scripts/05-fix-source.sh
+++ openwrt/scripts/05-fix-source.sh
@@ -58,7 +58,7 @@ fi
 
 # fix gcc-15.0.1 C23
 if [ "$USE_GCC15" = y ]; then
-    sed -i '/^ARCH:=i386/a KBUILD_CFLAGS += -std=gnu17\nEXTRA_CFLAGS += -std=gnu17\n$(info KBUILD_CFLAGS: $(KBUILD_CFLAGS))' target/linux/x86/Makefile
+    curl -s $mirror/openwrt/patch/openwrt-6.x/gcc-15-c23/999-fix-gcc15-c23-bool.patch > target/linux/x86/patches-6.6/999-fix-gcc15-c23-bool.patch
     # gmp
     mkdir -p package/libs/gmp/patches
     curl -s $mirror/openwrt/patch/openwrt-6.x/gcc-15-c23/gmp/001-fix-build-with-gcc-15.patch > package/libs/gmp/patches/001-fix-build-with-gcc-15.patch
