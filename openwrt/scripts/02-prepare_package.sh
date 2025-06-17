2e9bed91be228fc79da37bf5162508a15a6991f2
fattiger <nobody0036@proton.me>
2025-06-17 09:42:26 +0800

    package: add luci-app-openlist

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -81,9 +81,8 @@ git clone https://$github/sbwml/package_new_ftp package/new/ftp
 rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 git clone https://$github/sbwml/openwrt_helloworld package/new/helloworld -b v5
 
-# alist
-rm -rf feeds/packages/net/alist feeds/luci/applications/luci-app-alist
-git clone https://$github/sbwml/openwrt-alist package/new/alist
+# openlist
+git clone https://$github/sbwml/luci-app-openlist package/new/openlist
 
 # netdata
 sed -i 's/syslog/none/g' feeds/packages/admin/netdata/files/netdata.conf
