2925dc9761740f50e731c32d0ccbc38b864626d3
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-01-03 22:48:10 +0800

    zram

--- openwrt/scripts/01-prepare_base-mainline.sh
+++ openwrt/scripts/01-prepare_base-mainline.sh
@@ -5,21 +5,40 @@
 # autocore
 git clone https://$github/sbwml/autocore-arm -b openwrt-25.12 package/system/autocore
 
-# bpf-headers - 6.12
-# sed -ri "s/(PKG_PATCHVER:=)[^\"]*/\16.12/" package/kernel/bpf-headers/Makefile
-
-# x86_64 - target 6.12
-#curl -s $mirror/openwrt/patch/openwrt-6.x/x86/64/config-6.12 > target/linux/x86/64/config-6.12
-#curl -s $mirror/openwrt/patch/openwrt-6.x/x86/config-6.12 > target/linux/x86/config-6.12
-#mkdir -p target/linux/x86/patches-6.12
-#curl -s $mirror/openwrt/patch/openwrt-6.x/x86/patches-6.12/100-fix_cs5535_clockevt.patch > target/linux/x86/patches-6.12/100-fix_cs5535_clockevt.patch
-#curl -s $mirror/openwrt/patch/openwrt-6.x/x86/patches-6.12/103-pcengines_apu6_platform.patch > target/linux/x86/patches-6.12/103-pcengines_apu6_platform.patch
-# x86_64 - target
-#sed -ri "s/(KERNEL_PATCHVER:=)[^\"]*/\16.12/" target/linux/x86/Makefile
-#sed -i '/KERNEL_PATCHVER/a\KERNEL_TESTING_PATCHVER:=6.6' target/linux/x86/Makefile
-#curl -s $mirror/openwrt/patch/openwrt-6.x/x86/base-files/etc/board.d/01_leds > target/linux/x86/base-files/etc/board.d/01_leds
-#curl -s $mirror/openwrt/patch/openwrt-6.x/x86/base-files/etc/board.d/02_network > target/linux/x86/base-files/etc/board.d/02_network
+# kernel modules
+rm -rf package/kernel/linux
+git checkout package/kernel/linux
+pushd package/kernel/linux/modules
+    rm -f [a-z]*.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/block.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/bluetooth.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/can.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/crypto.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/firewire.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/fs.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/gpio.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/hwmon.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/i2c.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/iio.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/input.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/leds.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/lib.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/multiplexer.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netdevices.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netfilter.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netsupport.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/nls.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/other.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/pcmcia.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/rtc.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/sound.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/spi.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/usb.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/video.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/virt.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/w1.mk
+    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/wpan.mk
+popd
 
 # wireless-regdb
-#curl -s $mirror/openwrt/patch/openwrt-6.x/500-world-regd-5GHz.patch > package/firmware/wireless-regdb/patches/500-world-regd-5GHz.patch
-
+curl -s $mirror/openwrt/patch/openwrt-6.x/500-world-regd-5GHz.patch > package/firmware/wireless-regdb/patches/500-world-regd-5GHz.patch
