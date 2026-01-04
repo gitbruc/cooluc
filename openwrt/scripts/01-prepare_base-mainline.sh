94385345f83005b9a07de75ea90dec0f8fd6153f
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-01-04 11:45:22 +0800

    fix .mk file

--- openwrt/scripts/01-prepare_base-mainline.sh
+++ openwrt/scripts/01-prepare_base-mainline.sh
@@ -9,35 +9,35 @@ git clone https://$github/sbwml/autocore-arm -b openwrt-25.12 package/system/aut
 rm -rf package/kernel/linux
 git checkout package/kernel/linux
 pushd package/kernel/linux/modules
-    rm -f [a-z]*.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/block.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/bluetooth.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/can.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/crypto.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/firewire.mk
+    rm -f fs.mk iio.mk other.mk rtc.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/block.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/bluetooth.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/can.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/crypto.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/firewire.mk
     curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/fs.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/gpio.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/hwmon.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/i2c.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/gpio.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/hwmon.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/i2c.mk
     curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/iio.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/input.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/leds.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/lib.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/multiplexer.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netdevices.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netfilter.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netsupport.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/nls.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/input.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/leds.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/lib.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/multiplexer.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netdevices.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netfilter.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/netsupport.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/nls.mk
     curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/other.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/pcmcia.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/pcmcia.mk
     curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/rtc.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/sound.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/spi.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/usb.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/video.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/virt.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/w1.mk
-    curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/wpan.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/sound.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/spi.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/usb.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/video.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/virt.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/w1.mk
+    #curl -Os $mirror/openwrt/patch/openwrt-6.x/modules/wpan.mk
 popd
 
 # wireless-regdb
