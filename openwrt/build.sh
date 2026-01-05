cfe7d0696a97758751bb255644f598df5bfd67a2
gitxiuxiu <195287644+gitxiuxiu@users.noreply.github.com>
2026-01-05 11:37:17 +0800

    Remove user authorization check from build script
    Removed authorization check for user sbwml and git credentials.

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -20,13 +20,6 @@ endgroup() {
     GROUP=
 }
 
-# check
-if [ "$(whoami)" != "sbwml" ] && [ -z "$git_name" ] && [ -z "$git_password" ]; then
-    echo -e "\n${RED_COLOR} Not authorized. Execute the following command to provide authorization information:${RES}\n"
-    echo -e "${BLUE_COLOR} export git_name=your_username git_password=your_password${RES}\n"
-    exit 1
-fi
-
 #####################################
 #  NanoPi R4S OpenWrt Build Script  #
 #####################################
@@ -39,7 +32,7 @@ ip_info=`curl -sk https://ip.cooluc.com`;
 export mirror=https://init.cooluc.com
 
 # github actions - caddy server
-if [ "$(whoami)" = "runner" ] && [ "$git_name" != "private" ]; then
+if [ "$(whoami)" = "runner" ]; then
     export mirror=http://127.0.0.1:8080
 fi
 
