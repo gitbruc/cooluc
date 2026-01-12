7c341ae3564973ebfe83da52f3ef3736b0ac9865
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-01-12 23:10:29 +0800

    fix shortcut fe

--- openwrt/scripts/01-prepare_base-mainline.sh
+++ openwrt/scripts/01-prepare_base-mainline.sh
@@ -132,3 +132,4 @@ curl -s $mirror/openwrt/patch/kernel-6.12/net/983-add-bcm-fullcone-nft_masq-supp
 # shortcut-fe
 curl -s $mirror/openwrt/patch/kernel-6.12/net/601-netfilter-export-udp_get_timeouts-function.patch > target/linux/generic/hack-6.12/601-netfilter-export-udp_get_timeouts-function.patch
 curl -s $mirror/openwrt/patch/kernel-6.12/net/953-net-patch-linux-kernel-to-support-shortcut-fe.patch > target/linux/generic/hack-6.12/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+echo "CONFIG_KERNEL_SHORTCUT_FE=y" >> .config
