647792b8812a94ea1955b5e2a9465ffa02aea129
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-01-26 23:46:17 +0800

    add option squashfs zstd

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -332,7 +332,7 @@ if [ "$USE_GCC15" = "y" ] && [ "$ENABLE_CCACHE" = "y" ]; then
     tools_suffix="_ccache"
 fi
 
-# ZSTD compression ()
+# ZSTD compression
 if [ "$ENABLE_SquashFS_ZSTD" = "y" ]; then
     echo "CONFIG_TARGET_ROOTFS_SQUASHFS_ZSTD=y" >> .config
 fi
