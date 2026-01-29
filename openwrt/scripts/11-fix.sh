69e3db22e3b058a78df86bde4c1e2f7ff5fbb798
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-01-29 23:00:10 +0800

    fix smartdns hash

--- openwrt/scripts/11-fix.sh
+++ openwrt/scripts/11-fix.sh
@@ -6,7 +6,6 @@ sed -i 's/34c85d914e01006439f5e1c9287ae96d6bfcc729ed4bcf386bf5948b938254f4/5ef82
 
 # fix xray-core hash
 sed -i 's/6016f70297e7d63d2347e3ba0362ccc107c90a7f62b86cb4ee36ae4dab3eec42/c814c9b2e6c92e08d3db929792c56e2863a1a0e252c774ec048095efea6b67a1/g' package/new/helloworld/xray-core/Makefile
-#rm -rf package/new/helloworld/luci-app-passwall
 
 # intel-microcode
 sed -i 's|mkdir $(PKG_BUILD_DIR)/intel-ucode-ipkg|mkdir -p $(PKG_BUILD_DIR)/intel-ucode-ipkg|' package/firmware/intel-microcode/Makefile
