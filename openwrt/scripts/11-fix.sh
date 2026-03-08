917de55fe3b7907689ebdc2d3c499c18d165dbc7
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-03-08 15:37:47 +0800

    intel-microcode

--- openwrt/scripts/11-fix.sh
+++ openwrt/scripts/11-fix.sh
@@ -5,4 +5,4 @@ sed -i 's/227eef2dfffb56445145e7b8a76f6d6fa678ce3e99aceec58f7d35564f4cfafd/44017
 sed -i 's/34c85d914e01006439f5e1c9287ae96d6bfcc729ed4bcf386bf5948b938254f4/5ef82ea81d5f627f52171e3b487331ecdd270554555cbff3d291590e19f4658d/g' feeds/packages/net/smartdns/Makefile
 
 # intel-microcode
-sed -i 's|mkdir $(PKG_BUILD_DIR)/intel-ucode-ipkg|mkdir -p $(PKG_BUILD_DIR)/intel-ucode-ipkg|' package/firmware/intel-microcode/Makefile
+# sed -i 's|mkdir $(PKG_BUILD_DIR)/intel-ucode-ipkg|mkdir -p $(PKG_BUILD_DIR)/intel-ucode-ipkg|' package/firmware/intel-microcode/Makefile
