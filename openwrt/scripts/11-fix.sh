baae5cdd78abddfbf20f60dc47e10eae7a64208e
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-04-07 09:34:37 +0800

    1

--- openwrt/scripts/11-fix.sh
+++ openwrt/scripts/11-fix.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # fix smartdns hash
-#sed -i 's/227eef2dfffb56445145e7b8a76f6d6fa678ce3e99aceec58f7d35564f4cfafd/6ba61d10164ee61a16b2c7b930c9113598cbbf9595a531b372088e66546b27c3/g' feeds/packages/net/smartdns/Makefile
-#sed -i 's/34c85d914e01006439f5e1c9287ae96d6bfcc729ed4bcf386bf5948b938254f4/5ef82ea81d5f627f52171e3b487331ecdd270554555cbff3d291590e19f4658d/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/227eef2dfffb56445145e7b8a76f6d6fa678ce3e99aceec58f7d35564f4cfafd/6ba61d10164ee61a16b2c7b930c9113598cbbf9595a531b372088e66546b27c3/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/34c85d914e01006439f5e1c9287ae96d6bfcc729ed4bcf386bf5948b938254f4/5ef82ea81d5f627f52171e3b487331ecdd270554555cbff3d291590e19f4658d/g' feeds/packages/net/smartdns/Makefile
 
 # intel-microcode
 sed -i 's/mkdir $(PKG_BUILD_DIR)\/intel-ucode-ipkg/rm -rf $(PKG_BUILD_DIR)\/intel-ucode-ipkg \&\& mkdir -p $(PKG_BUILD_DIR)\/intel-ucode-ipkg/' package/firmware/intel-microcode/Makefile
