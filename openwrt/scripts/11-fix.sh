9f46ed984439b89ba243adac78621b9a80ec4086
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-05-07 16:01:55 +0800

    dockerd hash

--- openwrt/scripts/11-fix.sh
+++ openwrt/scripts/11-fix.sh
@@ -10,3 +10,5 @@ sed -i 's/mkdir $(PKG_BUILD_DIR)\/intel-ucode-ipkg/rm -rf $(PKG_BUILD_DIR)\/inte
 # rust
 sed -i 's/$(PYTHON) $(HOST_BUILD_DIR)\/x.py/env -u CI -u GITHUB_ACTIONS $(PYTHON) $(HOST_BUILD_DIR)\/x.py --set llvm.download-ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
+# dockerd
+sed -i 's/6850b0e5d07bed32b3613d4c7da50e0fc36542239a5ff5188b524494e9edda75/f2d4d892f5439ac8b3b28a2ba03d29db1a377f8dd5d057ca941cdbba92f6ed7f/' feeds/packages/utils/dockerd/Makefile
