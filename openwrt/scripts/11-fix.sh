3eee0491f9718cc18fe5adb04e8c1b50a3ff017a
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-05-15 10:49:00 +0800

    25.12.4

--- openwrt/scripts/11-fix.sh
+++ openwrt/scripts/11-fix.sh
@@ -4,9 +4,6 @@
 sed -i 's/227eef2dfffb56445145e7b8a76f6d6fa678ce3e99aceec58f7d35564f4cfafd/42e280cbfc168f88690652b85ce919c2aeedc9ef7c4b55122742e484b23492cd/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/34c85d914e01006439f5e1c9287ae96d6bfcc729ed4bcf386bf5948b938254f4/5ef82ea81d5f627f52171e3b487331ecdd270554555cbff3d291590e19f4658d/g' feeds/packages/net/smartdns/Makefile
 
-# intel-microcode
-#sed -i 's/mkdir $(PKG_BUILD_DIR)\/intel-ucode-ipkg/rm -rf $(PKG_BUILD_DIR)\/intel-ucode-ipkg \&\& mkdir -p $(PKG_BUILD_DIR)\/intel-ucode-ipkg/' package/firmware/intel-microcode/Makefile
-
 # rust
 #sed -i 's/$(PYTHON) $(HOST_BUILD_DIR)\/x.py/env -u CI -u GITHUB_ACTIONS $(PYTHON) $(HOST_BUILD_DIR)\/x.py --set llvm.download-ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
