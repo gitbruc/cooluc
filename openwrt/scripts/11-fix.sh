be35f2019dd999656417cead66dd501e8063ad72
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-05-15 15:15:11 +0800

    1

--- openwrt/scripts/11-fix.sh
+++ openwrt/scripts/11-fix.sh
@@ -8,7 +8,7 @@ sed -i 's/34c85d914e01006439f5e1c9287ae96d6bfcc729ed4bcf386bf5948b938254f4/5ef82
 #sed -i 's/$(PYTHON) $(HOST_BUILD_DIR)\/x.py/env -u CI -u GITHUB_ACTIONS $(PYTHON) $(HOST_BUILD_DIR)\/x.py --set llvm.download-ci-llvm=false/g' feeds/packages/lang/rust/Makefile
 
 # dockerd
-sed -i 's/406f6ba2f369e384e39bebe837859a888413dd71608ace7a9b0dc7d550dbd570/f2d4d892f5439ac8b3b28a2ba03d29db1a377f8dd5d057ca941cdbba92f6ed7f/' feeds/packages/utils/dockerd/Makefile
+#sed -i 's/406f6ba2f369e384e39bebe837859a888413dd71608ace7a9b0dc7d550dbd570/f2d4d892f5439ac8b3b28a2ba03d29db1a377f8dd5d057ca941cdbba92f6ed7f/' feeds/packages/utils/dockerd/Makefile
 
 # natflow
 curl -sL $mirror/openwrt/patch/natflow/999-fix-ipset-api-kernel-6.12.patch > package/new/natflow/patches/999-fix-ipset-api-kernel-6.12.patch
