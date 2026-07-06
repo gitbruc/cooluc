488515d9002b151c9042b3ae4735b430e5dd909d
Lawliet <110576223+gitbruc@users.noreply.github.com>
2026-07-06 16:34:27 +0800

    smartdns hash

--- openwrt/scripts/11-fix.sh
+++ openwrt/scripts/11-fix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # fix smartdns hash
-sed -i 's/227eef2dfffb56445145e7b8a76f6d6fa678ce3e99aceec58f7d35564f4cfafd/42e280cbfc168f88690652b85ce919c2aeedc9ef7c4b55122742e484b23492cd/g' feeds/packages/net/smartdns/Makefile
+sed -i 's/42e280cbfc168f88690652b85ce919c2aeedc9ef7c4b55122742e484b23492cd/skip/g' feeds/packages/net/smartdns/Makefile
 sed -i 's/34c85d914e01006439f5e1c9287ae96d6bfcc729ed4bcf386bf5948b938254f4/5ef82ea81d5f627f52171e3b487331ecdd270554555cbff3d291590e19f4658d/g' feeds/packages/net/smartdns/Makefile
 
 # rust
